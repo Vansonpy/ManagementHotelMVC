3d8db0c9c09fe2ad104646c688a88613114ed2b3
Van Son Py <vansonpy2002@gmail.com>
2023-04-13 11:04:34 +0700

    updateAddImg

--- src/main/resources/static/js/addRoom.js
+++ src/main/resources/static/js/addRoom.js
@@ -1,41 +1,52 @@
 const dropArea = document.querySelector(".drag-area");
 const drapText = dropArea.querySelector("header");
-const button = dropArea.querySelector("button");
+//const button = dropArea.querySelector("button");
+const bntFile = dropArea.querySelector(".bntFile");
 const input = document.querySelector("input[name=multipartFile]");
-
-button.addEventListener("click", function() {
+bntFile.addEventListener("click", function() {
 	input.click();
 })
 
 input.addEventListener("change", function(event) {
+	const file = event.target.files[0];
 	const fileReader = new FileReader();
-	const files = event.target.files;
-	fileReader.readAsDataURL(files[0])
+	fileReader.readAsDataURL(file)
 	fileReader.addEventListener("load", (event) => {
 		const img = event.target.result;
-		dropArea.innerHTML = `<img src = '${img}' style="width:100%" class="imgInner">`;
+		dropArea.innerHTML = `<img src = '${img}' style="width:100%" class="imgInner">
+				<div class="file">
+					<img
+						src="https://cdn3.iconfinder.com/data/icons/file-management-system-outline-1/32/file_manager_icon_set-22-512.png"
+						alt="" width="50%" height="50%">
+						<p><button class="bntFile" type="button">Choose file</button> or Drag</p>
+				</div>`;
 	})
 
 })
 dropArea.addEventListener("dragover", (event) => {
 	event.preventDefault();
-	drapText.textContent = "Upload File"
+	//drapText.textContent = "Upload File"
 })
 
 dropArea.addEventListener("dragleave", (event) => {
 	event.preventDefault();
-	drapText.textContent = "Pull Upload File"
+	//drapText.textContent = "Pull Upload File"
 })
 
 dropArea.addEventListener("drop", (event) => {
 	event.preventDefault();
 	const file = event.dataTransfer.files[0];
+	input.files = event.dataTransfer.files
 	const fileReader = new FileReader();
-	input.attributes.class.ownerElement.files[0];
-	//const files = event.target.files;
 	fileReader.readAsDataURL(file)
 	fileReader.addEventListener("load", (event) => {
 		const img = event.target.result;
-		dropArea.innerHTML = `<img src = '${img}' style="width:100%" class="imgInner">`;
+		dropArea.innerHTML = `<img src = '${img}' style="width:100%;" class="imgInner"> 
+					<div class="file">
+					<img
+						src="https://cdn3.iconfinder.com/data/icons/file-management-system-outline-1/32/file_manager_icon_set-22-512.png"
+						alt="" width="50%" height="50%">
+						<p><button class="bntFile" type="button">Choose file</button> or Drag</p>
+				</div>`;
 	})
 })
